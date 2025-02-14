c54e1f26e9064cad7309c205a76ced8b3ce6d4ea
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-02-14 16:38:58 +0530

    Update file_selector.py

--- bot/modules/file_selector.py
+++ bot/modules/file_selector.py
@@ -18,7 +18,7 @@ from bot.helper.telegram_helper.message_utils import (
 @new_task
 async def select(_, message):
     if not Config.BASE_URL:
-        await send_message(message, "Base URL not defined! ❗")
+        await send_message(message, "⚠️ Base URL not defined!")
         return
     user_id = message.from_user.id
     msg = message.text.split()
@@ -26,19 +26,19 @@ async def select(_, message):
         gid = msg[1]
         task = await get_task_by_gid(gid)
         if task is None:
-            await send_message(message, f"GID: <code>{gid}</code> Not Found. ❌")
+            await send_message(message, f"❌ GID: <code>{gid}</code> Not Found.")
             return
     elif reply_to_id := message.reply_to_message_id:
         async with task_dict_lock:
             task = task_dict.get(reply_to_id)
         if task is None:
-            await send_message(message, "This is not an active task! 🚫")
+            await send_message(message, "⚠️ This is not an active task!")
             return
     elif len(msg) == 1:
         msg = (
-            "Reply to an active /cmd which was used to start the download or add gid along with cmd 📝\n\n"
-            + "This command mainly for selection in case you decided to select files from an already added torrent. "
-            + "But you can always use /cmd with arg `s` to select files before download starts. 📂"
+            "⚠️ Reply to an active /cmd which was used to start the download or add gid along with cmd\n\n"
+            + "📌 This command mainly for selection incase you decided to select files from already added torrent. "
+            + "🚫 But you can always use /cmd with arg `s` to select files before download start."
         )
         await send_message(message, msg)
         return
@@ -46,7 +46,7 @@ async def select(_, message):
     if user_id not in (Config.OWNER_ID, task.listener.user_id) and (
         user_id not in user_data or not user_data[user_id].get("is_sudo")
     ):
-        await send_message(message, "This task is not for you! 🚫")
+        await send_message(message, "This task is not for you!")
         return
     if await task.status() not in [
         MirrorStatus.STATUS_DOWNLOAD,
@@ -55,11 +55,11 @@ async def select(_, message):
     ]:
         await send_message(
             message,
-            "Task should be in download or pause (in case message deleted by mistake) or queued status (in case you have used torrent file)! ⚠️",
+             "⏳ Task should be in download, paused (if message was deleted by mistake), or queued status (if you used a torrent file)!",
         )
         return
     if task.name().startswith("[METADATA]") or task.name().startswith("Trying"):
-        await send_message(message, "Try after downloading metadata finished! ⏳")
+        await send_message(message, "📡 Try again after the metadata download is complete!")
         return
 
     try:
@@ -74,15 +74,15 @@ async def select(_, message):
                     await TorrentManager.aria2.forcePause(id_)
                 except Exception as e:
                     LOGGER.error(
-                        f"{e} Error in pause, this mostly happens after abuse aria2",
+                        f"⚠️ {e} Error in pause, likely due to Aria2 abuse.")
                     )
         task.listener.select = True
     except Exception:
-        await send_message(message, "This is not a bittorrent task! ❌")
+        await send_message(message, "❌ This is not a BitTorrent task!")
         return
 
     SBUTTONS = bt_selection_buttons(id_)
-    msg = "⏸️ Your download is paused. Choose files, then press **Done Selecting** to resume downloading. ⏬"
+     msg = "⏸️ Your download is paused. Choose files, then press **Done Selecting** to resume downloading. ⏬"
     await send_message(message, msg, SBUTTONS)
 
 
@@ -93,11 +93,11 @@ async def confirm_selection(_, query):
     message = query.message
     task = await get_task_by_gid(data[2])
     if task is None:
-        await query.answer("This task has been cancelled! ❌", show_alert=True)
+        await query.answer("❌ This task has been cancelled!", show_alert=True)
         await delete_message(message)
         return
     if user_id != task.listener.user_id:
-        await query.answer("This task is not for you! 🚫", show_alert=True)
+        await query.answer("This task is not for you!", show_alert=True)
     elif data[1] == "pin":
         await query.answer(data[3], show_alert=True)
     elif data[1] == "done":
@@ -130,10 +130,10 @@ async def confirm_selection(_, query):
                         await TorrentManager.aria2.unpause(id_)
                     except Exception as e:
                         LOGGER.error(
-                             f"⚠️ {e} Error in resume, likely due to Aria2 abuse. Try selecting again!",
+                            f"⚠️ {e} Error in resume, likely due to Aria2 abuse. Try selecting again!",
                         )
         await send_status_message(message)
         await delete_message(message)
     else:
         await delete_message(message)
-        await task.cancel_task() ❌
+        await task.cancel_task()
