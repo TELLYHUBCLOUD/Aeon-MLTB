0cefe49e7db51fe5905ec2285c71b087db1441f1
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-02-14 16:27:21 +0530

    Update gd_count.py

--- bot/modules/gd_count.py
+++ bot/modules/gd_count.py
@@ -19,7 +19,7 @@ async def count_node(_, message):
         link = reply_to.text.split(maxsplit=1)[0].strip()
 
     if is_gdrive_link(link):
-        msg = await send_message(message, f"Counting: <code>{link}</code>")
+        msg = await send_message(message, f"🔍 **Counting:** <code>{link}</code>...")
         name, mime_type, size, files, folders = await sync_to_async(
             GoogleDriveCount().count,
             link,
@@ -29,14 +29,14 @@ async def count_node(_, message):
             await send_message(message, name)
             return
         await delete_message(msg)
-        msg = f"<b>Name: </b><code>{name}</code>"
-        msg += f"\n\n<b>Size: </b>{get_readable_file_size(size)}"
-        msg += f"\n\n<b>Type: </b>{mime_type}"
+        msg = f"<b>Name: </b><code>{name}</code> 📄"
+        msg += f"\n\n<b>Size: </b>{get_readable_file_size(size)} 📏"
+        msg += f"\n\n<b>Type: </b>{mime_type} 🗂️"
         if mime_type == "Folder":
-            msg += f"\n<b>SubFolders: </b>{folders}"
-            msg += f"\n<b>Files: </b>{files}"
-        msg += f"\n\n<b>cc: </b>{tag}"
+            msg += f"\n<b>SubFolders: </b>{folders} 📁"
+            msg += f"\n<b>Files: </b>{files} 📂"
+        msg += f"\n\n<b>cc: </b>{tag} 🙋"
     else:
-        msg = "Send Gdrive link along with command or by replying to the link by command"
+        msg = "⚠️ **Send a Gdrive link along with the command or reply to the link with the command!**"
 
     await send_message(message, msg)
