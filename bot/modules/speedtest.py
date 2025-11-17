02fc9eb20b86376cde4d0894b24aa06ff92dce29
5hojib <yesiamshojib@gmail.com>
2025-11-17 07:21:14 +0000

    format: auto-format code by ruff.
    Signed-off-by: 5hojib <yesiamshojib@gmail.com>

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -1,4 +1,5 @@
 from speedtest import Speedtest
+
 from bot import LOGGER
 from bot.core.aeon_client import TgClient
 from bot.helper.ext_utils.bot_utils import new_task
@@ -8,15 +9,19 @@ from bot.helper.telegram_helper.message_utils import (
     edit_message,
     send_message,
 )
+
+
 @new_task
 async def speedtest(_, message):
     speed = await send_message(message, "⚡ Initializing Speedtest...")
+
     def get_speedtest_results():
         test = Speedtest()
         test.get_best_server()
         test.download()
         test.upload()
         return test.results
+
     result = await TgClient.bot.loop.run_in_executor(None, get_speedtest_results)
     if not result:
         await edit_message(speed, "❌ Speedtest failed to complete.")
@@ -27,7 +32,7 @@ async def speedtest(_, message):
 <blockquote>📡 <b>Ping:</b> <code>{result.ping} ms</code>
 📤 <b>Upload:</b> <code>{get_readable_file_size(result.upload / 8)}/s</code>
 📥 <b>Download:</b> <code>{get_readable_file_size(result.download / 8)}/s</code>
-🌐 <b>IP Address:</b> <code>{result.client['ip']}</code></blockquote>
+🌐 <b>IP Address:</b> <code>{result.client["ip"]}</code></blockquote>
 """
     try:
         await send_message(message, string_speed, photo=result.share())
