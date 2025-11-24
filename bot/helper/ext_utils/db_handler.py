5176bdd7e457663e04cbfb802c728aebe7ebbafb
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-11-24 20:57:49 +0530

    Update db_handler.py

--- bot/helper/ext_utils/db_handler.py
+++ bot/helper/ext_utils/db_handler.py
@@ -29,7 +29,7 @@ class DbManager:
                 Config.DATABASE_URL,
                 server_api=ServerApi("1"),
             )
-            dbid = Config.DATABASE_NUM
+            dbid = TgClient.ID
             self.db = self._conn[f"tellyaeon{dbid}"]
             self._return = False
             LOGGER.info("Successfully connected to the database.")
