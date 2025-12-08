053786f8a67f0a34fe5899f4b77a1b8807cde1b1
5hojib <yesiamshojib@gmail.com>
2025-12-08 10:24:22 +0000

    format: auto-format code by ruff.
    Signed-off-by: 5hojib <yesiamshojib@gmail.com>

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -40,10 +40,10 @@ from bot.helper.ext_utils.status_utils import get_readable_file_size
 from bot.helper.ext_utils.task_manager import check_running_tasks, start_from_queued
 from bot.helper.mirror_leech_utils.gdrive_utils.upload import GoogleDriveUpload
 from bot.helper.mirror_leech_utils.rclone_utils.transfer import RcloneTransferHelper
+from bot.helper.mirror_leech_utils.status_utils.ffmpeg_status import FFmpegStatus
 from bot.helper.mirror_leech_utils.status_utils.gdrive_status import (
     GoogleDriveStatus,
 )
-from bot.helper.mirror_leech_utils.status_utils.ffmpeg_status import FFmpegStatus
 from bot.helper.mirror_leech_utils.status_utils.queue_status import QueueStatus
 from bot.helper.mirror_leech_utils.status_utils.rclone_status import RcloneStatus
 from bot.helper.mirror_leech_utils.status_utils.telegram_status import TelegramStatus
