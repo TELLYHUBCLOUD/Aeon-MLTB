9afd515451a6489039c0de461ff2b986489a915e
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-12-16 18:54:31 +0530

    Update update.py

--- update.py
+++ update.py
@@ -110,7 +110,7 @@ UPSTREAM_REPO = (
 )
 
 UPSTREAM_BRANCH = (
-    config_file.get("UPSTREAM_BRANCH", "betax")
+    config_file.get("UPSTREAM_BRANCH", "betax2")
     or os.getenv("UPSTREAM_BRANCH", "")
     or "main"
 )
