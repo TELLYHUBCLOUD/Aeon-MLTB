d5b0ccf1acbcdc87cf9d2ae9048a5546fe745008
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-12-17 08:42:10 +0530

    Update status_utils.py
    Mising import

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -3,7 +3,7 @@ from asyncio import gather, iscoroutinefunction
 from html import escape
 from time import time
 
-from psutil import disk_usage, virtual_memory
+from psutil import cpu_percent, disk_usage, virtual_memory
 
 from bot import DOWNLOAD_DIR, bot_start_time, status_dict, task_dict, task_dict_lock
 from bot.helper.telegram_helper.button_build import ButtonMaker
