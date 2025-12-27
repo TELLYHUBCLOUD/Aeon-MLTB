1d30c3f30e0f06d2b4bb517ca9a676a27d4a97d0
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-12-27 11:45:32 +0530

    feat: introduce `TgClient` class for managing Telegram bot and user clients.

--- bot/core/aeon_client.py
+++ bot/core/aeon_client.py
@@ -28,7 +28,7 @@ class TgClient:
             bot_token=Config.BOT_TOKEN,
             workdir="/usr/src/app",
             parse_mode=enums.ParseMode.HTML,
-            max_message_cache_size=0,
+            message_cache_size=0,
             sleep_threshold=60,
             #    max_concurrent_transmissions=100,
         )
@@ -49,7 +49,7 @@ class TgClient:
                     parse_mode=enums.ParseMode.HTML,
                     no_updates=True,
                     max_concurrent_transmissions=100,
-                    max_message_cache_size=0,
+                    message_cache_size=0,
                     sleep_threshold=60,
                 )
                 await cls.user.start()
